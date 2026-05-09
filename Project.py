dbfec204e68c6d56743933ca280fb6b66c43e111
Makayla Henry <Makaylahenry524@gmail.com>
2026-05-09 10:42:19 -0500

    added player class and stuff

--- Project.py
+++ Project.py
@@ -19,3 +19,31 @@ YELLOW = (255, 255, 0)
 PURPLE = (128, 0, 128)
 
 font = pygame.font.SysFont("Arial", 28)
+
+class Player:
+    self.rect = pygame.Rect(100, 450, 50, 70)
+    self.vel_y = 0
+    self.speed = 6
+    self.jump_power = -16
+    self.on_ground = False
+    self.health = 100
+    self.dragging = False
+
+def move(self, keys):
+    dx = 0
+
+    if keys[pygame.K_LEFT]:
+        dx = self.speed
+    if keys[pygame.K_RIGHT]:
+        dx = self.speed
+
+    self.rect.x += dx
+
+def gravity(self):
+    self.vel_y += 1
+    self.rect.y += self.vel_y
+
+    if self.rect.y >= 450:
+        self.rect.y = 450
+        self.vel_y = 0
+        self.on_ground = True
