0671b87f410b99cade734722460e732f54e69e01
Makayla Henry <Makaylahenry524@gmail.com>
2026-05-09 10:26:21 -0500

    added game screen

--- Project.py
+++ Project.py
@@ -1,3 +1,9 @@
 import pygame
 import random
 import math
+
+pygame.init()
+
+WIDTH, HEIGHT = 1000, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Gojo Vs Sukuna!")
