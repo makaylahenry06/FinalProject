94d98770d3b6cad5accde2d9db63b1f3fa4caac8
Makayla Henry <Makaylahenry524@gmail.com>
2026-05-09 10:32:13 -0500

    added color

--- Project.py
+++ Project.py
@@ -7,3 +7,15 @@ pygame.init()
 WIDTH, HEIGHT = 1000, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Gojo Vs Sukuna!")
+
+clock = pygame.time.clock()
+
+WHITE = (255, 255, 255)
+BLUE = (100, 149, 237)
+GREEN = (34, 177, 76)
+RED = (200, 0, 0)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+PURPLE = (128, 0, 128)
+
+font = pygame.font.SysFont("Arial", 28)
